d816c8e8df799913eba6f2ac497df1d3d977bb2c
RissGL <zhangdeyan792@gmail.com>
2026-05-29 03:35:01 +0800

    射击粒子

--- Assets/Scripts/Weapon/ShootingSystem.cs
+++ Assets/Scripts/Weapon/ShootingSystem.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
+using DG.Tweening;
+using System;
+using Cinemachine;
 
-namespace Assets.Scripts.Weapon
+public class ShootingSystem
 {
-    internal class ShootingSystem
+    private InputDataSo inputDataSo;
+    private InkSystem inkSystem;
+    private ParticleSystem particleSystem;
+    private Transform playerControllerTrans;
+    private Transform splatGunNozzle;
+
+    private bool wasShootInput=false;
+    private CinemachineImpulseSource impulseSource;
+
+    public ShootingSystem(InputDataSo inputDataSo, InkSystem inkSystem, ParticleSystem particleSystem,
+        Transform playerController,Transform splatGunNozzle)
     {
+        this.inputDataSo = inputDataSo;
+        this.inkSystem = inkSystem;
+        this.particleSystem = particleSystem;
+        this.playerControllerTrans = playerController;
+        this.splatGunNozzle = splatGunNozzle;
+        impulseSource = playerControllerTrans.GetComponent<CinemachineImpulseSource>();
+    }
+
+    public void Update()
+    {
+        if (inputDataSo.shootInput&&wasShootInput!=inputDataSo.shootInput)
+        {
+            wasShootInput = true;
+            Shoot();
+        }
+        else if (!inputDataSo.shootInput&&wasShootInput!=inputDataSo.shootInput)
+        {
+            wasShootInput = false;
+            StopShooting();
+        }
+    }
+
+    private void Shoot()
+    {
+        particleSystem.Play();
+    }
+    private void StopShooting()
+    {
+        particleSystem.Stop();
+    }
+
+    private void VisualPolish() 
+    {
+        if (!DOTween.IsTweening(playerControllerTrans))
+        {
+            playerControllerTrans.DOComplete();
+
+            Vector3 localPosition=playerControllerTrans.localPosition;
+
+            playerControllerTrans.DOLocalMove(localPosition - new Vector3(0, 0, .2f), .03f)
+                .OnComplete(() => playerControllerTrans.DOLocalMove(localPosition, .1f).SetEase(Ease.OutSine));
+
+            impulseSource.GenerateImpulse();
+        }
+
+        if (!DOTween.IsTweening(splatGunNozzle))
+        {
+            splatGunNozzle.DOComplete();
+            splatGunNozzle.DOPunchScale(new Vector3(0, 1, 1) / 1.5f, .15f, 10, 1);
+        }
     }
 }
